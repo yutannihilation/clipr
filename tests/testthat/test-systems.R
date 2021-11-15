452651377ea846783ec614cf57a4fefd0f9f111e
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2021-11-15 08:42:17 -0500

    Add headless clipboard tests for ubuntu

--- tests/testthat/test-systems.R
+++ tests/testthat/test-systems.R
@@ -2,22 +2,22 @@ context("systems")
 
 test_that("utility checking works on Linux-like", {
 
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "xclip")) {
+  if (identical(Sys.getenv("CLIP_TYPE"), "xclip")) {
     expect_true(has_xclip())
     expect_false(has_xsel())
   }
 
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "xsel")) {
+  if (identical(Sys.getenv("CLIP_TYPE"), "xsel")) {
     expect_false(has_xclip())
     expect_true(has_xsel())
   }
 
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "none")) {
+  if (identical(Sys.getenv("CLIP_TYPE"), "none")) {
     expect_false(has_xclip())
     expect_false(has_xsel())
   }
 
-  if (identical(Sys.getenv("TRAVIS_CLIP"), "nodisplay")) {
+  if (identical(Sys.getenv("CLIP_TYPE"), "nodisplay")) {
     expect_error(has_xclip())
     expect_false(has_xsel())
   }
